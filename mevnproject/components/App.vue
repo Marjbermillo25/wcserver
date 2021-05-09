325b8ca312afd2465dde15e0b9b4a720355309ba
Marjorie Bermillo <mbermillo.cscsoc@gmail.com>
2021-05-09 14:31:53 +0800

    First commit

---
+++ mevnproject/components/App.vue
@@ -0,0 +1,29 @@
+App.vue
+<template>
+    <div id ="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HomeComponent/>
+    </div>
+</template>
+
+<script>
+import HomeComponent from './components/HomeComponent.vue';
+
+export default {
+    name:'App',
+    components: {
+        HomeComponent
+    }
+}
+</script>
+
+<style>
+#app{
+    font-family: Avenir, Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing:antialiased;
+    -moz-osx-font-smoothing:grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
